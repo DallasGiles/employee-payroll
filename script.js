86b62b5724bb25adb242a51cf62e9585ca0b73fa
Dallas Giles <dallascgiles@gmail.com>
2024-03-21 18:22:35 -0600

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-
+const employee = { firstName: '', lastName:'', salary: 0};
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  const employeesArray = [ 'firstName', 'lastName', 0];
+  const employee = { firstName: '', lastName:'', salary: 0};
+  employee.firstName = prompt('What is the first name?');
+  employee.lastName = prompt ('What is the last name?');
+  employee.salary = prompt ('What is the salary?');
+  employeesArray.push(employee);
+
+  continueInput = prompt('Would you like to add another employee?');
+ 
+  console.log(employeesArray);
+  return employeesArray;
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  let total= 0;
+  for (let i=0; i < employeesArray.length; i++){
+    total+= employeesArray[i].salary
+  }
+  let avg = total / employeesArray.length;
+  return avg
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  if (employeesArray.length === 0){
+    return 'No employees recorded.';
+  }
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+  return employeesArray[randomIndex];
 }
 
 /*
@@ -44,18 +66,19 @@ const displayEmployees = function(employeesArray) {
     lastNameCell.textContent = currentEmployee.lastName;
     newTableRow.append(lastNameCell);
 
-    const salaryCell = document.createElement("td");
-    // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
-      style:"currency",
-      currency:"USD"
-    });
-
+    
     newTableRow.append(salaryCell);
 
     employeeTable.append(newTableRow);
   }
 }
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = employee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
 
 const trackEmployeeData = function() {
   const employees = collectEmployees();
